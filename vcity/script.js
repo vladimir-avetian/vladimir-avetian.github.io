31da90a951973d561497d23d8480cdaf51a595ad
Vladimir Avetian <93351884+vladimir-avetian@users.noreply.github.com>
2024-01-17 00:04:25 +0100

    Add files via upload

--- vcity/script.js
+++ vcity/script.js
@@ -9,5 +9,24 @@ $(document).ready(function() {
     $('.change-image').click(function() {
         var newImage = $(this).data('image');
         $('#interactive-image').attr('src', newImage);
+
+        var arrText = "";
+        switch(newImage) {
+            case 'paris_buildings_1e.png':
+                arrText = "1e Arrondissement de Paris";
+                break;
+            case 'paris_buildings_2e.png':
+                arrText = "2e Arrondissement de Paris";
+                break;
+            case 'paris_buildings_3e.png':
+                arrText = "3e Arrondissement de Paris";
+                break;
+            case 'paris_buildings_15e.png':
+                arrText = "15e Arrondissement de Paris";
+                break;
+            default:
+                arrText = "Paris";
+        }
+        $('#overlay-text').text(arrText);
     });
 });
